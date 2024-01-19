062baf1b64262d0601a2b74d5a8677ed76474370
opilane <opilane@HP-CZC2349HTQ>
2024-01-19 12:09:15 +0200

    Add project files.

---
+++ PythonApplication1.py
@@ -0,0 +1,29 @@
+print("Tere! Olen sinu uus sõber - Python")
+nimi= input("Mis on sinu nimi?")
+print(nimi +"oi kui ilus nimi!")
+print(nimi + "Kas leian Sinu keha indeksi? 0-ei, 1-jah =>")
+vastus=int(input())
+if arv ==1:
+    print(jah)
+else:
+    print("viga")
+if arv<16:
+    print("Tervisele ohtlik alakaal")
+if 16 <arv <=19:
+    print("Alakaal")
+if arv 19 - 25:
+    print("Normaalkaal")
+if arv 25 - 30:
+    print("Ülekaal")
+if arv 30 - 35:
+    print("Rasvumine")
+if arv 35 - 40:
+    print("Tugev rasvumine")
+if arv >40:
+    print("Tervisele ohtlik rasvumine")
+    else:
+print("Kahju! See on väga kasulik info!")
+try:
+   a=float(input("A "))
+  except:
+   ValueError
